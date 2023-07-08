d7729cfc762f84e087104b111722a9f839cca3db
2-dean <limsjs@naver.com>
2023-07-08 17:30:00 +0900

    검증2 - Bean Validation 완료

--- src/main/java/hello/itemservice/web/validation/VaildationItemApiController.java
+++ src/main/java/hello/itemservice/web/validation/VaildationItemApiController.java
@@ -16,7 +16,7 @@ public class VaildationItemApiController {
 
     @PostMapping("/add")
     public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
-        log.info("API 컨트롤러 호출");
+        log.info("API 컨트롤러 호출!");
 
         if(bindingResult.hasErrors()) {
             log.info("검증 오류 발생 errors={}", bindingResult);
